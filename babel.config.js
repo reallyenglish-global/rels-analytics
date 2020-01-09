58e6d958bf13d866ccf222de8a34594003798a83
randym <digital.ipseity@gmail.com>
2020-01-09 16:38:36 +0900

    fix(modernize): configure babel and use latest rels-gulp

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,26 @@
 const config = {
-  presets: ['@babel/preset-env'],
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        debug: true,
+        targets: {
+          browsers: [
+            'Explorer 11',
+            'last 1 Chrome versions',
+            'last 1 FireFox versions',
+            'last 1 Safari versions',
+            'last 1 Edge versions',
+          ],
+        },
+        useBuiltIns: 'usage',
+        corejs: {
+          version: '3',
+          proposals: 'true',
+        },
+      },
+    ],
+  ],
 }
 
 const cache = process.env.CI !== true
